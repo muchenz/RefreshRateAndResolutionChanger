4d6fab525ffd52f27942cdf32346605aa02d3a53
p c <muchenz@poczta.onet.pl>
2025-05-01 17:09:57 +0200

    add material

---
+++ RefreshRateAndResolutionChanger/Resources/Styles/CustomMenu.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace RefreshRateAndResolutionChanger.Resources.Styles
+{
+    public class CustomMenuItem : MenuItem
+    {
+        public static readonly DependencyProperty SubmenuItemHeightProperty =
+            DependencyProperty.Register("SubmenuItemHeight", typeof(int), typeof(CustomMenuItem),
+                new PropertyMetadata(0));
+        //new FrameworkPropertyMetadata(24, FrameworkPropertyMetadataOptions.Inherits));
+
+        // for acces XALM
+        public int SubmenuItemHeight
+        {
+            get { return (int)GetValue(SubmenuItemHeightProperty); }
+            set { SetValue(SubmenuItemHeightProperty, value); }
+        }
+    }
+
+
+    public class CustomMenu : Menu
+    {
+        public static readonly DependencyProperty SubmenuItemHeightProperty =
+            DependencyProperty.Register("SubmenuItemHeight", typeof(int), typeof(CustomMenu), new PropertyMetadata(0));
+
+        // for acces XALM
+
+        public int SubmenuItemHeight
+        {
+            get { return (int)GetValue(SubmenuItemHeightProperty); }
+            set { SetValue(SubmenuItemHeightProperty, value); }
+        }
+
+    }
+}
---
+++ RefreshRateAndResolutionChanger/Resources/Styles/Test/CustomMenuItem.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Media;
+
+namespace RefreshRateAndResolutionChanger.Resources.Styles.Test
+{
+        
+
+    public static class MyMarginCustom
+    {
+        public static readonly DependencyProperty MyMarginProperty =
+            DependencyProperty.RegisterAttached("MyMargin", typeof(Thickness), typeof(MyMarginCustom),
+                new FrameworkPropertyMetadata(new Thickness(0, 16, 0, 16), FrameworkPropertyMetadataOptions.Inherits));
+
+        // for acces XALM
+
+        public static Thickness GetMyMargin(DependencyObject obj) => (Thickness)obj.GetValue(MyMarginProperty);
+
+        public static void SetMyMargin(DependencyObject obj, Thickness value) => obj.SetValue(MyMarginProperty, value);
+
+    }
+
+    public class MyTestCustomControl2 : Control
+    {
+        static  MyTestCustomControl2()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(MyTestCustomControl2),
+           new FrameworkPropertyMetadata(typeof(MyTestCustomControl2)));
+        }
+        public static readonly DependencyProperty MyMargin2Property =
+            DependencyProperty.Register("MyMargin2", typeof(Thickness), typeof(MyTestCustomControl2),
+                new FrameworkPropertyMetadata(new Thickness(16, 16, 16, 16), FrameworkPropertyMetadataOptions.Inherits));
+
+        // for acces XALM
+
+
+        public Thickness MyMargin2
+        {
+            get { return (Thickness)GetValue(MyMargin2Property); }
+            set { SetValue(MyMargin2Property, value); }
+        }
+
+    }
+}
